52b5a8cfe7c95c85323224078194eaee919c325c
THONWELLING <thonwellingdani13@gmail.com>
2022-07-08 16:46:45 -0300

    entrando no chat

--- public/main.js
+++ public/main.js
@@ -1,1 +1,23 @@
 const socket = io();
+
+let userName = "";
+let userList = [];
+
+let loginPage = document.querySelector("#loginPage");
+let chatPage = document.querySelector("#chatPage");
+
+let loginInput = document.querySelector("#loginNameInput");
+let textInput = document.querySelector("#chatTextInput");
+
+loginPage.style.display = "flex";
+chatPage.style.display = "none";
+
+loginInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    let name = loginInput.value.trim();
+    if (name != "") {
+      userName = name;
+      document.title = "Chat (" + userName + ")";
+    }
+  }
+});
